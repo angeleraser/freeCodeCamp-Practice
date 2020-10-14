b7a184258f7614420a0608cf9a22a648bc1ff85e
angeleraser <angeleraser@gmail.com>
2020-10-14 17:23:33 -0400

    Added new functions to solve freeCodeCamp challenges

--- index.js
+++ index.js
@@ -22,11 +22,58 @@ function getRating(watchList) {
 }
 
 const squareList = (arr) => {
-  // Only change code below this line
   const validIntegers = arr.filter((val) => val % 1 === 0 && val >= 0);
   return validIntegers.map((val) => Math.pow(val, 2));
-  // Only change code above this line
 };
 
-const squaredIntegers = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
-console.log(squaredIntegers);
+function sentensify(str) {
+  const splittedString = str.split(/\W/gi);
+  return splittedString.join(" ");
+}
+
+function urlSlug(title) {
+  const url = title
+    .split(/\W/gi)
+    .filter((word) => word.length)
+    .map((word) => word.toLowerCase())
+    .reduce(
+      (text, word, index, arr) =>
+        text + `${word}${index !== arr.length - 1 ? "-" : ""}`,
+      ""
+    );
+  return url;
+}
+
+function add(x) {
+  return (y) => (z) => x + y + z;
+}
+
+//Impartial function
+function impartial(x, y, z) {
+  return x + y + z;
+}
+var partialFn = impartial.bind(this, 1, 2);
+partialFn(10); // Returns 13
+
+Object.prototype[Symbol.iterator] = function* () {
+  const keys = Object.keys(this);
+  for (const key of keys) {
+    yield this[key];
+  }
+};
+
+function whatIsInAName(collection, source) {
+  const FILTERED_OBJECTS = collection.filter((obj) => {
+    const objKeys = Object.keys(obj),
+      sourceKeys = Object.keys(source);
+    if (objKeys.length === sourceKeys.length) {
+      return objKeys.every((key) => obj[key] === source[key]);
+    } else {
+      return (
+        objKeys.some((key) => obj[key] === source[key]) &&
+        objKeys.length >= sourceKeys.length
+      );
+    }
+  });
+  return FILTERED_OBJECTS;
+}
